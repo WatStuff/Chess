091024ea3a210d0de708fa17650c6bbc4b6bad0c
Wat01 <41803850+Wat01@users.noreply.github.com>
2018-11-16 22:42:59 +0100

    Add files via upload
    This is the main file. You have to compile and then run it.

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+
+using namespace std;
+
+char board1[][8] ={	{'t','h','b','k','q','b','h','t'},
+			{'p','p','p','p','p','p','p','p'},
+			{' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ',' ',' ',' ',' ',' ',' ',' '},
+			{'P','P','P','P','P','P','P','P'},
+			{'T','H','B','K','Q','B','H','T'}};
+			
+char board[][8] = {	{'t',' ','b','t','q','b',' ','t'},
+			{' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ',' ',' ',' ',' ',' ',' ',' '},
+			{' ',' ',' ',' ',' ',' ',' ',' '},
+			{'T',' ','B','K','Q','B',' ','T'}};
+			
+#include</home/wat1/Programming/C++/projects/chess/mainP/funct.h>
+
+
+int main()
+{
+	int player = 1;
+	int start[2];
+	char st_aux;
+	int end[2];
+	player = 1;
+	bool rslt;
+	while(1==1)
+	{	
+		current_board(player);
+		cout<<endl<<"\t\t\t\t\t     ---------"<<endl;
+		cout<<"\t\t\t\t\t     |player"<<player<<"|"<<endl;
+		cout<<"\t\t\t\t\t     ---------"<<endl;
+		cout<<endl<<"write the start position: ";
+		cin>>st_aux;
+		cin>>start[0];
+		start[1] = ch(st_aux);
+		if(start[1]==8){continue;}
+		start[0] = 7 - (start[0]-1);
+		cout<<board[start[0]][start[1]];
+		cout<<endl<<"write the end position: ";
+		cin>>st_aux;
+		end[1] = ch(st_aux);
+		cin>>end[0];
+		end[0] = 7 - (end[0]-1);
+		cout<<board[end[0]][end[1]];
+		if(check_if_av(start,end)==true)
+		{
+			rslt = manager(start,end,player);
+			if(rslt==false){continue;}
+		}
+		else{continue;}
+
+
+		if(player==1){player = 2;continue;}
+		if(player==2){player = 1;continue;}
+	}
+
+
+	return 0;
+}
